b65f8665d622603ea4bd0ed307288c4d34f34c9c
micekey <micekey24oct@gmail.com>
2024-01-22 00:33:12 +0530

    Created Header and linked the pages to it

--- AuthApp/src/App.jsx
+++ AuthApp/src/App.jsx
@@ -4,17 +4,19 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/about' element={<About />}></Route>
-        <Route path='/profile' element={<Profile />}></Route>
-        <Route path='/signin' element={<SignIn />}></Route>
-        <Route path='/signup' element={<SignUp />}></Route>
-      </Routes>
+      <Header/>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
+          <Route path='/signin' element={<SignIn />}></Route>
+          <Route path='/signup' element={<SignUp />}></Route>
+        </Routes>
     </BrowserRouter>
   )
 }
